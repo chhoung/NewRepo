9bcf30a2f56aabfb0eb067a8c5895a6d4ad2b950
chhoung <11ien@phengs-MacBook.local>
2016-10-06 11:38:58 +0700

    Ive made some changes

--- NewRepo/ViewController.swift
+++ NewRepo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("I made some useless change")
     }
 
     override func didReceiveMemoryWarning() {
